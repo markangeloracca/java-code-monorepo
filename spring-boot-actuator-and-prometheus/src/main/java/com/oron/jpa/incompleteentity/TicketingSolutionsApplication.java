129849d815e2d11355161015ac53b9dd77a4176a
markangeloracca <markangelo.racca@gmail.com>
2023-04-19 01:00:33 +0800

    Working project with actuator and prometheus

---
+++ spring-boot-actuator-and-prometheus/src/main/java/com/oron/jpa/incompleteentity/TicketingSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.oron.jpa.incompleteentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSolutionsApplication.class, args);
+	}
+
+}
